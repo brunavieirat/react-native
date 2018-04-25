22b4ce822afce1d9ff42d1ca42ffeb9aafd94350
Bruna Vieira <bruna.vieira.t@hotmail.com>
2018-04-24 21:48:17 -0300

    efetuaLogin

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,45 +1,78 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Dimensions,
-  ScrollView,
-  Button,
-  TextInput
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Dimensions,
+    ScrollView,
+    Button,
+    TextInput
 } from 'react-native';
 
-export default class Login extends Component{
+export default class Login extends Component {
 
-    constructor(){
+    constructor() {
         super()
-        this.state={
+        this.state = {
             usuario: '',
             senha: ''
         }
     }
-    render(){
 
-        return(
+    efetuaLogin = () => {
+
+        const request = {
+            method: 'POST',
+            body: JSON.stringify({
+                login: this.state.usuario,
+                senha: this.state.senha
+            }),
+            headers: new Headers({
+                "Content-type": "application/json"
+            })
+
+        }
+
+        const uri='https://instalura-api.herokuapp.com/api/public/login'
+
+      //  const uri = 'http://192.168.0.137:8080/api/public/login'
+
+        fetch(uri, request)
+            .then(response => {
+                if (!response.ok)
+                    throw new Error('Não deu certo. Arruma!');
+                return response.text()
+            })
+            .then(token => console.warn(token))
+            .catch(error => {
+            })
+    }
+
+    render() {
+
+        return (
             <View style={styles.container}>
 
+                <Image source={require('../../resources/img/send.png')} />
                 <View style={styles.form}>
-                <TextInput style={styles.input}
-                placeholder="Usuário..."
-                onChangeText={texto => this.setState({usuario: texto})}
-                />
+                    <TextInput style={styles.input}
+                        placeholder="Usuário..."
+                        onChangeText={texto => this.setState({ usuario: texto })}
+                    />
 
-                <TextInput style={styles.input}
-                placeholder="Digite sua senha"
-                onChangeText={texto => this.setState({senha: texto})}
-                />
+                    <TextInput style={styles.input}
+                        autoCapitalize="none"
+                        secureTextEntry={true}
+                        placeholder="Digite sua senha"
+                        onChangeText={texto => this.setState({ senha: texto })}
+                    />
 
-                <Button title="Login"
-                onPress={() => console.warn('oook')} />
-            </View>
+                    <Button title="Login"
+                        onPress={this.efetuaLogin} />
                 </View>
+            </View>
 
         )
     }
@@ -48,15 +81,15 @@ export default class Login extends Component{
 const styles = StyleSheet.create({
     input: {
         height: 40,
-       // borderBottom: '#ddd'
-       
+        // borderBottom: '#ddd'
+
     },
-    container:{
+    container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'        
+        justifyContent: 'center'
     },
-    form:{
+    form: {
         width: Dimensions.get('screen').width * 0.8
     }
 
